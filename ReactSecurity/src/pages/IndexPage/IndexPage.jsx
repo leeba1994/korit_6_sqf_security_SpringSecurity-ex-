3e2e1e64ad1f4ebf0fc8d82cfbab552418fe1fec
leeba1994 <ds5y2kk@naver.com>
2024-08-30 17:55:41 +0900

    240830_수업추가

--- ReactSecurity/src/pages/IndexPage/IndexPage.jsx
+++ ReactSecurity/src/pages/IndexPage/IndexPage.jsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import React from 'react';
 import { useQueryClient } from 'react-query';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 /** @jsxImportSource @emotion/react */
 
 const layout = css`
@@ -75,29 +75,110 @@ const rightBox = css`
     }
 `;
 
+const userInfoBox = css`
+    display: flex;  
+    justify-content: flex-start;
+    width: 100%;
+
+    `;
+
+const profileImgBox = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 64px;
+    height: 64px;
+    box-shadow: 0px 0px 2px #00000088;
+
+`;
+
+const profileInfo = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    flex-grow: 1;
+    padding: 10px;
+
+    & > button {
+        box-sizing: border-box;
+        border: 1px solid #dbdbdb;
+        border-radius: 37px;
+        padding: 5px 10px;
+        height: 37px;
+        background-color: #ffffff;
+        color: #555555;
+        font-size: 16px;
+        cursor: pointer;
+
+    }
+
+`;
+
 function IndexPage(props) {
+    const navigate = useNavigate(); // useNavigate를 사용하면 페이지 이동이 가능하다.
+
     const queryClient = useQueryClient();
-    const data = queryClient.getQueryData("accessTokenValidQuery");
+    //const data = queryClient.getQueryData("userInfoQuery"); // 쿼리의 데이터를 가져온다.
+    const userInfoState = queryClient.getQueryState("userInfoQuery"); // 쿼리의 상태를 가져온다.
+    const accessTokenValidState = queryClient.getQueryState("accessTokenValidQuery"); // 쿼리의 상태를 가져온다.
     // console.log(data);
-    queryClient.invalidateQueries(); // 지금까지 들고온 쿼리를 만료시킨다. 만료되면 다시 실행
+    // console.log(state);
+
+    // queryClient.invalidateQueries(); // 지금까지 들고온 쿼리를 만료시킨다. 만료되면 다시 실행
+
+    const handleLoginButtonOnClick = () => {
+        navigate("/user/login");    // 페이지 이동
+    }
+
+    const handleLogoutButtonOnClick = () => {
+        localStorage.removeItem("accessToken");
+        window.location.replace("/");
+    }
 
     return (
         <div css={layout}>
             <header css={header}>
                 <input type="search" placeholder='검색어를 입력해 주세요.' />
             </header>
-            <main css={main}>
-                <div css={leftBox}></div>
-                <div css={rightBox}>
-                    <p>더 안전하고 편리하게 이용하세요</p>
-                    <button><Link to={"/user/login"}>로그인</Link></button>
-                    <div>
-                        <Link to={"/user/help/id"}>아이디 찾기</Link>
-                        <Link to={"/user/help/pw"}>비밀번호 찾기</Link>
-                        <Link to={"/user/join"}>회원가입</Link>
-                    </div>
-                </div>
-            </main>
+            {
+                accessTokenValidState.status === "idle" || accessTokenValidState.status === "loading"
+                ? <h1>로딩중....</h1>
+                :
+                <main css={main}>
+                    <div css={leftBox}></div>
+                    {
+                        userInfoState.status === "success" &&
+                        <div css={rightBox}>
+                            <div css={userInfoBox}>
+                                <div css={profileImgBox}>
+                                    <img src="" alt="" />
+                                </div>
+                                <div css={profileInfo}>
+                                    <div>
+                                        <div>{userInfoState.data.data.username}님</div>
+                                        <div>{userInfoState.data.data.email}</div>
+                                    </div>
+                                    <button onClick={handleLogoutButtonOnClick}>로그아웃</button>
+                                </div>
+                            </div>
+                        </div>
+                    }  
+                    {
+                        userInfoState.status !== "success" &&
+                        <div css={rightBox}>
+                            <p>더 안전하고 편리하게 이용하세요</p>
+                            <button onClick={handleLoginButtonOnClick}>로그인</button>  
+                            <div>
+                                <Link to={"/user/help/id"}>아이디 찾기</Link>  
+                                <Link to={"/user/help/pw"}>비밀번호 찾기</Link> 
+                                <Link to={"/user/join"}>회원가입</Link>
+                            </div>
+                        </div>
+                    }
+                </main>
+            }
         </div>
     );
 }
--- SpringSecurityMybatis/src/main/java/com/study/SpringSecurityMybatis/service/UserService.java
+++ SpringSecurityMybatis/src/main/java/com/study/SpringSecurityMybatis/service/UserService.java
@@ -5,6 +5,7 @@ import com.study.SpringSecurityMybatis.dto.request.ReqSignupDto;
 import com.study.SpringSecurityMybatis.dto.response.RespDeleteUserDto;
 import com.study.SpringSecurityMybatis.dto.response.RespSigninDto;
 import com.study.SpringSecurityMybatis.dto.response.RespSignupDto;
+import com.study.SpringSecurityMybatis.dto.response.RespUserInfoDto;
 import com.study.SpringSecurityMybatis.entity.Role;
 import com.study.SpringSecurityMybatis.entity.User;
 import com.study.SpringSecurityMybatis.entity.UserRoles;
@@ -26,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -120,6 +122,21 @@ public class UserService {
                 .isDeleting(true)
                 .message("사용자 삭제 완료")
                 .deletedUser(user)
+                .build();
+    }
+
+    public RespUserInfoDto getUserInfo(Long id) {
+        User user = userMapper.findById(id);
+        Set<String> roles = user.getUserRoles().stream().map(
+                userRole -> userRole.getRole().getName()
+        ).collect(Collectors.toSet());
+
+        return RespUserInfoDto.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .email(user.getEmail())
+                .roles(roles)
                 .build();
     }
 }
